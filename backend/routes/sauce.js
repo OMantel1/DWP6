124f304324ad7793fcddcf43648ff1a2e0dab92d
Olivier <olivier@macbook-air-de-olivier.home>
2020-04-18 15:29:54 +0200

    Ajoute route sauce

---
+++ backend/routes/sauce.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const sauceCtrl = require('../controllers/sauce');
+
+router.get('/sauces', sauceCtrl.getAllSauces);
+router.get('/sauces/:id', sauceCtrl.getOneSauce);
+router.post('/sauces', sauceCtrl.createSauce);
+router.put('/sauces/:id', sauceCtrl.modifySauce);
+router.delete('/sauce/:id', sauceCtrl.deleteSauce);
+
+module.exports = router;
