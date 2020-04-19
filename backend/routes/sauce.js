6961a84908073711fbf1ddc55be17d9de5156452
Olivier <olivier@macbook-air-de-olivier.home>
2020-04-19 16:19:47 +0200

    Modifie routes

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,6 +6,6 @@ router.get('/sauces', sauceCtrl.getAllSauces);
 router.get('/sauces/:id', sauceCtrl.getOneSauce);
 router.post('/sauces', sauceCtrl.createSauce);
 router.put('/sauces/:id', sauceCtrl.modifySauce);
-router.delete('/sauce/:id', sauceCtrl.deleteSauce);
+router.delete('/sauces/:id', sauceCtrl.deleteSauce);
 
 module.exports = router;
