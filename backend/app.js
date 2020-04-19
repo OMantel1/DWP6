9b40142241a6d63f020bebce2faeb754cfea3756
Olivier <olivier@macbook-air-de-olivier.home>
2020-04-19 16:16:38 +0200

    Ajoute et configure mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose= require('mongoose');
+
+const saucesRoutes = require('./routes/sauce');
+
 const app = express();
 
-app.use((req, res) => {
-res.json({ message: 'Votre requete à bien été recue !!!'})
-})
+mongoose.connect('mongodb+srv://OMuser1:pwdatabgk8@cluster0-9uleb.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à mongoDb ok !'))
+    .catch(() => console.log('Connexion à mongoDb echouée !!'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/sauces', saucesRoutes);
 
 module.exports = app;
